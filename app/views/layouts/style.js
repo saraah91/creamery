eef80c768724708aa6c7f30e31ff1a848df57583
Sara <smohanad@andrew.cmu.edu>
2019-03-29 14:04:50 +0000

    improved date input feature

--- app/views/layouts/style.js
+++ app/views/layouts/style.js
@@ -1,11 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginRight = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(234,249,254)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "rgba(249,209,171)";
 }
