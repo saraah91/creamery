a18f86d1e421247075dc49e8eae8d6142494aca1
Sara <smohanad@andrew.cmu.edu>
2019-04-17 21:06:17 +0000

    edited flavor and user models

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -3,4 +3,15 @@ class Flavor < ApplicationRecord
     #Relationships
     has_many :store_flavors
     has_many :stores, through: :store_flavors
+    
+	#Validations
+	validates_presence_of :name
+	
+	#Scopes
+    scope :active,          -> { where(active: true) }
+    scope :inactive,        -> { where(active: false) }
+    scope :alphabetical,    -> { order('last_name, first_name') }
+
+    
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     #Relationships
     belongs_to :employee
     
+    #Validations
+    validates_format_of :email, :with => ['/^\d{0-9}[@]d{0-9}$/'], :message => "email must be in the right";
+    validates_uniqueness_of :email
+    
+    
 end
