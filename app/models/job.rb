824f45898a650e0afc7894fb07dbef2e5bc62997
Sara <smohanad@andrew.cmu.edu>
2019-04-17 20:58:20 +0000

    specified relationships + added scopes & methods in shifts, jobs, and shift_job models

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,16 @@ class Job < ApplicationRecord
     #Relationships
     has_many :shift_jobs
 	has_many :shifts, through: :shift_jobs
+	
+	#Validations
+	validates_presence_of :name
+	
+	#Scopes
+    scope :active,          -> { where(active: true) }
+    scope :inactive,        -> { where(active: false) }
+    scope :alphabetical,    -> { order('last_name, first_name') }
+
+
+
+	
 end
